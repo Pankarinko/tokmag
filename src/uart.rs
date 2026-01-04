14876ffa265f0032621e33b73982d6dd47361ff3
Pankarinko <pankarinko@gmail.com>
2026-01-04 02:47:27 +0100

    Initialize LCR

--- src/uart.rs
+++ src/uart.rs
@@ -1,10 +1,15 @@
-const BASE: *const usize = 0x10000000 as *const usize;
-const RBR: *const usize = BASE;
+const BASE: *const u8 = 0x10000000 as *const u8;
+const RBR: *const u8 = BASE;
 const LSR_OFFSET: usize = 5;
 const LCR_OFFSET: usize = 3;
 const FCR_OFFSET: usize = 2;
 const THR_OFFSET: usize = 0;
 
-fn read_byte() {}
+fn init_uart() {
+    unsafe {
+        let lcr: *mut u8 = (BASE as *mut u8).add(LCR_OFFSET);
+        *lcr = 0x43;
+    }
+}
 
 fn write_byte() {}
