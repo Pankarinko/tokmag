2214a0608b49dd74501989e781365f55afffef4d
Pankarinko <pankarinko@gmail.com>
2026-01-04 02:19:04 +0100

    Add constants for uart

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 mod panic_handler;
+mod uart;
 
 #[unsafe(no_mangle)]
 fn r_entry() {
---
+++ src/uart.rs
@@ -0,0 +1,10 @@
+const BASE: *const usize = 0x10000000 as *const usize;
+const RBR: *const usize = BASE;
+const LSR_OFFSET: usize = 5;
+const LCR_OFFSET: usize = 3;
+const FCR_OFFSET: usize = 2;
+const THR_OFFSET: usize = 0;
+
+fn read_byte() {}
+
+fn write_byte() {}
