ba12b8d4e31b3d292938a021ec0e774c7752bf82
Pankarinko <pankarinko@gmail.com>
2026-01-17 16:44:22 +0100

    Make writes volatile

--- src/uart.rs
+++ src/uart.rs
@@ -1,3 +1,5 @@
+use core::ptr::write_volatile;
+
 const BASE: *const u8 = 0x10000000 as *const u8;
 const RBR: *const u8 = BASE;
 const LSR_OFFSET: usize = 5;
@@ -12,15 +14,16 @@ const THR_OFFSET: usize = 0;
 fn init_uart() {
     unsafe {
         let lcr: *mut u8 = (BASE as *mut u8).add(LCR_OFFSET);
-        *lcr = 0x43;
+        write_volatile(lcr, 0x43);
         let fcr: *mut u8 = (BASE as *mut u8).add(FCR_OFFSET);
-        *fcr = 0x0;
+        write_volatile(fcr, 0);
     }
 }
 
 fn write_byte(c: u8) {
     let rbr: *mut u8 = RBR as *mut u8;
     unsafe {
-        while *(BASE as *mut u8).add(LCR_OFFSET) & 0x10 == 0x10 {};
-        *rbr = c };
+        while *(BASE as *mut u8).add(LCR_OFFSET) & 0x10 == 0x10 {}
+        write_volatile(rbr, c)
+    };
 }
