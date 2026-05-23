0c3beb7811d29b9667bdfcec6f460f5e044a3a10
Pankarinko <pankarinko@gmail.com>
2026-05-24 00:55:37 +0200

    Fix linker problems

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 #![no_std]
 #![no_main]
+
 mod panic_handler;
 mod uart;
+use core::arch::global_asm;
+use core::include_str;
+global_asm!(include_str!("../entry/entry.S"));
 
 #[unsafe(no_mangle)]
 fn r_entry() {
+    //::write_text("Yo\n");
+    loop {};
     panic!();
 }
+
+
